0d5721efec372ee8d939225de31f590e64fa09a4
Faxe <152980783+JonasEHartig@users.noreply.github.com>
2026-07-07 03:23:54 +0200

    Station spawning logic revamped, added separate CalculateStationPlacement() returning tuple

--- backend/Draw.cs
+++ backend/Draw.cs
@@ -12,9 +12,11 @@ public class Draw
     {
         Raylib.InitWindow(800, 480, "Train Simulation");
         
+    
+
         System.Timers.Timer sec3Timer = new System.Timers.Timer();
         sec3Timer.Elapsed += new ElapsedEventHandler(AddStation);
-        sec3Timer.Interval = 3000;
+        sec3Timer.Interval = 500;
         sec3Timer.Enabled = true;
 
         while (!Raylib.WindowShouldClose())
@@ -23,10 +25,13 @@ public class Draw
 
             Raylib.DrawText("Train Simulation", 12, 12, 20, Color.White);
 
-
-            foreach (Station currentStation in StationList)
+            foreach (Station currentStation in StationList.ToList())
             {
-                Raylib.DrawCircle(currentStation.StationXPosition, currentStation.StationYPosition, 10, Color.Green);
+                int RectangleWidth = currentStation.StationXCoverArea2 - currentStation.StationXCoverArea1;
+                int RectangleHeight = currentStation.StationYCoverArea2 - currentStation.StationYCoverArea1;
+                Raylib.DrawRectangleGradientV(currentStation.StationXCoverArea1, currentStation.StationYCoverArea1, RectangleWidth, RectangleHeight, Color.Red, Color.DarkGray);
+                Raylib.DrawCircle(currentStation.StationXPosition, currentStation.StationYPosition, 10, Color.Blue);
+
             }
 
             Raylib.EndDrawing();
@@ -36,6 +41,39 @@ public class Draw
     }
 
     public void AddStation(object source, ElapsedEventArgs e)
+    {
+        (int stationXPosition, int stationYPosition, int stationXCoverArea1, int stationXCoverArea2, int stationYCoverArea1,int stationYCoverArea2) = CalculateStationPlacement();
+        bool addStationBool = false;
+        
+        Station station = new Station(stationXPosition, stationYPosition, stationXCoverArea1, stationXCoverArea2, stationYCoverArea1, stationYCoverArea2);
+
+        if (!StationList.Any())
+        {
+            addStationBool = true;
+
+        }
+
+        foreach (Station currentStation in StationList)
+        {
+            if (stationXPosition <= currentStation.StationXCoverArea1 & stationXPosition >= currentStation.StationXCoverArea2 & stationYPosition <= currentStation.StationYCoverArea1 & stationYPosition >= currentStation.StationYCoverArea2)
+            {
+                addStationBool = false;
+                AddStation(this, null);
+            }
+            else
+            {
+                addStationBool = true;
+                break;
+            }
+        }
+
+        if (addStationBool == true)
+        {
+            StationList.Add(station);
+        }
+    }
+
+    public Tuple<int, int, int, int, int, int> CalculateStationPlacement()
     {
         Random rng = new Random();
 
@@ -48,22 +86,6 @@ public class Draw
         int stationYCoverArea1 = stationYPosition - 50;
         int stationYCoverArea2 = stationYPosition + 50;
 
-        foreach (Station currentStation in StationList)
-        {
-            if (stationXPosition <= currentStation.StationXCoverArea1 & stationXPosition >= currentStation.StationXCoverArea2)
-            {
-                
-            }
-
-            if (stationYPosition <= currentStation.StationYCoverArea1 & stationYPosition >= currentStation.StationYCoverArea2)
-            {
-                
-            }
-        }
-
-
-        Station station = new Station(stationXPosition, stationYPosition, stationXCoverArea1, stationXCoverArea2, stationYCoverArea1, stationYCoverArea2);
-
-        StationList.Add(station);
+        return Tuple.Create(stationXPosition, stationYPosition, stationXCoverArea1, stationXCoverArea2, stationYCoverArea1, stationYCoverArea2);
     }
 }
